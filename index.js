031e1e458e526b618e6deb05eaba7a88ae5713d0
arpita-bhattacharya01 <arpitabhattacharya01@outlook.com>
2025-06-27 11:33:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ import nodemailer from 'nodemailer';
 import fs from 'fs';
 import multer from 'multer';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -36,6 +41,12 @@ const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.resolve(__dirname, "dist"))); 
+
+app.get(/^(?!\/api).*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 // Global error handler to prevent crashes
 process.on('uncaughtException', (error) => {
